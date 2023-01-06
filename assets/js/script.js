a37446fd5b00605df90d46684ade62527d9b2d2d
kikppvr <prapavarine.c@gmail.com>
2023-01-07 01:44:13 +0700

    adjust header

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/*-- Scroll Up/Down add class --*/
+var scrollPosition = 0;
+
+$(window).scroll(function (e) {
+
+    var currentPosition = $(this).scrollTop();
+
+    if (currentPosition > scrollPosition) {
+
+        $('.header').addClass('scroll-down');
+        $('.header').removeClass('scroll-up');
+    } else {
+
+        $('.header').addClass('scroll-up');
+        $('.header').removeClass('scroll-down');
+    }
+
+    if (currentPosition === 0) {
+        $('.header').removeClass('scroll-up');
+    }
+
+    scrollPosition = currentPosition;
+});
