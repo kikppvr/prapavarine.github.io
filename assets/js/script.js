55122f9ae33dd598212d3e3d0095449e7e4290f7
kikppvr <prapavarine.c@gmail.com>
2023-01-14 01:12:03 +0700

    adjusts header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,33 @@
 /*-- Scroll Up/Down add class --*/
-var scrollPosition = 0;
 
 $(window).scroll(function (e) {
+
+    var scrollPosition = 0;
 
     var currentPosition = $(this).scrollTop();
 
     if (currentPosition > scrollPosition) {
-
-        $('.header').addClass('scroll-down');
-        $('.header').removeClass('scroll-up');
-    } else {
-
-        $('.header').addClass('scroll-up');
-        $('.header').removeClass('scroll-down');
+        $('.header').addClass('header--black');
     }
 
     if (currentPosition === 0) {
-        $('.header').removeClass('scroll-up');
+        $('.header').removeClass('header--black');
     }
 
     scrollPosition = currentPosition;
 });
 
 
+$(document).ready(function () {
+
+    if ($(window).width() <= 768) {
+        $('.navbar-collapse').on('show.bs.collapse', function () {
+            console.log('test');
+            $('.header').addClass('header--on-open');
+        });
+
+        $('.navbar-collapse').on('hidden.bs.collapse', function () {
+            $('.header').removeClass('header--on-open');
+        });
+    }
+});
